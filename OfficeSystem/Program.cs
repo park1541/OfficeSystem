2f302d746a339bbcce2c8c0888427f83317df9cc
park1541 <a059771@gmail.com>
2026-07-01 07:22:01 +0900

    chore: 기본 템플릿 파일 정리 및 프로젝트 설정

--- OfficeSystem/Program.cs
+++ OfficeSystem/Program.cs
@@ -1,11 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using OfficeSystem.Data;
+using OfficeSystem.Services;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<OfficeDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
-
+builder.Services.AddScoped<IUserService, UserService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
